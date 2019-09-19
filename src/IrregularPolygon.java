b6d48d73977ebb1e5c3a231243d9b0f3323cde61
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-19 15:24:01 +0530

    adding class file with proper indentation

--- src/IrregularPolygon.java
+++ src/IrregularPolygon.java
@@ -1,16 +1,16 @@
-class IrregularPolygon extends PolygonAngle implements Polygon {
-		
-		private int perimeter=0;
 
-		public int calculatePerimeter(int noOfSides){
-		
-		this.noOfSides=noOfSides;
-		System.out.print("\n Enter length of each sides::");
-			for(int i=0;i<this.noOfSides;i++){
-			length=scan.nextInt();
-			perimeter=perimeter+length;		
-			}
-		return perimeter;
-		}
+import java.util.Scanner;
 
-	}
+public class IrregularPolygon extends PolygonAngle implements Polygon {
+    private int perimeter=0;
+    Scanner scan=new Scanner(System.in);
+    public int calculatePerimeter(int noOfSides){
+        this.noOfSides=noOfSides;
+        System.out.print("\n Enter length of each sides::");
+        for(int i=0;i<this.noOfSides;i++){
+            length=scan.nextInt();
+            perimeter=perimeter+length;
+        }
+        return perimeter;
+    }
+}
