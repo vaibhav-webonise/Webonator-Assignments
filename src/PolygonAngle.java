a062b2abea1d8a3004e95618b37b64592933d0d0
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-19 15:28:18 +0530

    adding this class file with proper indentation

--- src/PolygonAngle.java
+++ src/PolygonAngle.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-class PolygonAngle{
-		private int defaultAngle=180;
-		private int noOfTriangle;
-		private int sumOfInteriorAngles;
-		protected int noOfSides;
-		protected int length;
-		Scanner scan=new Scanner(System.in);
 
-		public int calculateSumOfAngles(int noOfSides){
-			
-			this.noOfSides=noOfSides;
-			noOfTriangle=noOfSides-2;
-			sumOfInteriorAngles=noOfTriangle*defaultAngle;
-		    return sumOfInteriorAngles;
-		}
-		
-	}
-	
+public class PolygonAngle {
+    private int defaultAngle=180;
+    private int noOfTriangle;
+    private int sumOfInteriorAngles;
+    protected int noOfSides;
+    protected int length;
+    Scanner scan=new Scanner(System.in);
+
+    public int calculateSumOfAngles(int noOfSides){
+        this.noOfSides=noOfSides;
+        noOfTriangle=noOfSides-2;
+        sumOfInteriorAngles=noOfTriangle*defaultAngle;
+        return sumOfInteriorAngles;
+    }
+}
