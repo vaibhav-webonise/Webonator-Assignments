cd623fc775c297c3e22ce969ec191e3fe0a7b689
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-19 15:27:00 +0530

    adding this class file with proper indentation

--- src/RegularPolygon.java
+++ src/RegularPolygon.java
@@ -1,14 +1,11 @@
+import java.util.Scanner;
 
-	class RegularPolygon  extends PolygonAngle implements Polygon{
-	
-		public int calculatePerimeter(int noOfSides){
-		this.noOfSides=noOfSides;
-		System.out.print("\n Enter the length for side::");
-		length=scan.nextInt();
-		return this.noOfSides*length;
-		}
-
-		
-		
-		
-	}
+public class RegularPolygon extends PolygonAngle implements Polygon{
+    Scanner scan=new Scanner(System.in);
+    public int calculatePerimeter(int noOfSides){
+        this.noOfSides=noOfSides;
+        System.out.print("\n Enter the length for side::");
+        length=scan.nextInt();
+        return this.noOfSides*length;
+    }
+}
