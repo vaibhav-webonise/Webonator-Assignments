b7bc9ec45139bd5cf3deee507d493d7cf1f91845
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-20 13:19:17 +0530

    java file to find duplicates in string

---
+++ src/com/company/StringDemo.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class StringDemo {
+
+    void removeDuplicatesInString(){
+        String inputString="I came I Saw I conquered I went";
+        StringBuilder outputString = new StringBuilder();
+        Set<Character> linkedHashSet = new LinkedHashSet<>();
+        for (int i = 0; i < inputString.length(); i++) {
+            linkedHashSet.add(inputString.charAt(i));
+        }
+
+        for (Character c : linkedHashSet) {
+            outputString.append(c);
+        }
+
+        System.out.println("String after removing duplicates::"+outputString);
+    }
+}
