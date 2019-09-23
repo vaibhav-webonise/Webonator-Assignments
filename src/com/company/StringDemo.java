ffd3a68152a89ce31934faa1d2cb4b257c142529
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-23 18:55:57 +0530

    Updated StringDemo java file

--- src/com/company/StringDemo.java
+++ src/com/company/StringDemo.java
@@ -1,11 +1,11 @@
 package com.company;
 
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Arrays;
 
 class StringDemo {
     void removeDuplicatesInString() {
         String inputString = "I came I Saw I conquered I went";
-
         LinkedHashSet<String> distinctStrings = new LinkedHashSet<>(Arrays.asList(inputString.split(" ")));
         for (String outputString : distinctStrings) {
             System.out.print(" " + outputString);
