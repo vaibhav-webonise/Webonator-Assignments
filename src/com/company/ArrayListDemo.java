9c79218c542abe31c7bfb93d581ed44ba0cc1c2f
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-23 17:48:15 +0530

    updated ArrayListDemo java file

--- src/com/company/ArrayListDemo.java
+++ src/com/company/ArrayListDemo.java
@@ -1,22 +1,21 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class ArrayListDemo {
-    ArrayList<String> arrayList = new ArrayList<>();
-    Scanner scanInputFromUser = new Scanner(System.in);
-    int defaultHobbiesCount = 4;
+    private List<String> hobbies = new ArrayList<>();
+    private Scanner scanInputFromUser = new Scanner(System.in);
+    private static final int DEFAULT_HOBBIES_COUNT = 4;
 
     void traverseArrayList() {
         System.out.print("\n Let us know your four hobbies::");
-        for (int i = 0; i < defaultHobbiesCount; i++) {
-            String hobbies = scanInputFromUser.next();
-            arrayList.add(hobbies);
+        for (int i = 0; i < DEFAULT_HOBBIES_COUNT; i++) {
+            hobbies.add(scanInputFromUser.next());
         }
-        for (String showHobbies : arrayList
-        ) {
-            System.out.print("   " + showHobbies);
+        for (String hobby : hobbies) {
+            System.out.print("   " + hobby);
         }
     }
 }
