7644d66fe3709b174a9a3a07ecd1bdce2f29d808
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-09-25 18:08:39 +0530

    adding Helloworld java file

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+    private String message;
+    public  void setMessage(String message){
+        this.message=message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
